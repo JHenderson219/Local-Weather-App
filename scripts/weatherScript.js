016f7f11e794e6ec12000baea337e96dd9f6ee04
JHenderson219 <hendersonj219@gmail.com>
2016-10-18 14:32:10 -0700

    JSON update
    Getting JSON from API is working

--- scripts/weatherScript.js
+++ scripts/weatherScript.js
@@ -1,13 +1,22 @@
-$(document).ready(function() { 
-/*$.getJson("api.openweathermap.org/data/2.5/weather?lat="+clientLat+"&lon="+clientlon,"1edd9d579e483c8e11d172bd53b1ac31",function(){	
-	});*/
+$(document).ready(function() {
+	var clientLat= 35.2828;
+	var clientLon = -120.6596;
+	$("#location").empty().append("<h3 class = 'text-center '>Your Current Location </h3>")
 	$("#farenheit").on("click", function(){
-		$("#temperature").html("Temp in farenheit");
+		$(".temperature").empty().append("<h3 class = 'text-center'>Temp in farenheit</h3>");
 	});
 	$("#celsius").on("click", function(){
-		$("#temperature").html("Temp in celsius");
+		$(".temperature").empty().append("<h3 class = 'text-center'>Temp in celsius</h3>");
 	});
-	var weatherJson = "";
-	$.getJson("api.openweathermap.org/data/2.5/weather?lat="+"35"+"&lon="+"35","1edd9d579e483c8e11d172bd53b1ac31",function(){
+/*var weatherJSON = $.ajax({
+	url: "http://api.openweathermap.org/data/2.5/weather?lat="+clientLat+"&lon="+clientLon,
+	type: "GET",
+	dataType: "json",
+	success: function(){$("#jsonTest").empty().append("<h3>"+weatherJSON+"</h3>"); },
+	beforeSend: setHeader
+});*/
+
+var weatherJson = $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat="+clientLat+"&lon="+clientLon, "APPID=77488f9dd9ff84cf5e598bdf84999e77", function(){
+	$("#jsonTest").html(JSON.stringify(weatherJson));
 	});
 });
