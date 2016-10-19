dd1919c1f990e35f03a1ccb977cf2570ced5958d
Jordan Henderson <hendersonj219@gmail.com>
2016-10-19 11:16:56 -0700

    Geolocation works!

--- scripts/weatherScript.js
+++ scripts/weatherScript.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-	
 	var clientLat;
 	var clientLon;
-	if (Navigator.geolocation) {
-  		Navigator.geolocation.getCurrentPosition(function(position) {
+	if (navigator.geolocation) {
+  		navigator.geolocation.getCurrentPosition(function(position) {
     	clientLat = position.coords.latitude;
     	clientLon = position.coords.longitude;
+    	$("#location").html("<h3 class = 'text-center '>"+clientLat+' '+clientLon+"</h3>")
     	console.log(clientLat);
 		console.log(clientLon);
   		});
@@ -14,7 +14,7 @@ $(document).ready(function() {
   		}
 	
 	
-	$("#location").empty().append("<h3 class = 'text-center '>Your Current Location </h3>")
+	
 	$("#farenheit").on("click", function(){
 		$(".temperature").empty().append("<h3 class = 'text-center'>Temp in farenheit</h3>");
 	});
@@ -28,9 +28,7 @@ $(document).ready(function() {
 	success: function(){$("#jsonTest").empty().append("<h3>"+weatherJSON+"</h3>"); },
 	beforeSend: setHeader
 });*/
-setPos;
-console.log(clientLat);
-console.log(clientLon);
+
 var weatherJson = $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat="+clientLat+"&lon="+clientLon, "APPID=77488f9dd9ff84cf5e598bdf84999e77", function(){
 	$("#temperature").html("<h3 class= 'text-center animated fadeIn'>" +"99"+"</h3>"); // CHANGE 99 TO VAR FOR TEMP
 	$("#location").html("<h3 class='text-center animated fadeIn'>"+ "Location_Name" + "</h3>"); //CHANGE LOCATION_NAME TO VAR FOR LOCATION
